0059f861eea4aecd58f307a6541b5f225e010b1d
Jinad <140488745+jinadInduwithwa@users.noreply.github.com>
2025-07-01 11:38:17 +0530

    initial project configuration

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h2 className="text-red-500 ">ecommerce site</h2>
+      <Button>demo button</Button>
+
+    </div>
+    
+  );
+}
